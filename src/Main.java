3639ce9cce02f9dafaf4a3e176e8c483157a64bc
catsbi <killfine1988@gmail.com>
2020-08-31 22:57:30 +0900

    23. interpreter pattern까지

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,23 @@
-import command.command.MacroCommand;
-import command.drawer.DrawCanvas;
-import command.drawer.DrawCommand;
+import interpreter.*;
 
-import javax.swing.*;
-import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-public class Main extends JFrame implements ActionListener {
-    private MacroCommand history = new MacroCommand();
-    private DrawCanvas canvas = new DrawCanvas(400, 400, history);
-    private JButton clearButton = new JButton("clear");
-    private JButton undoButton = new JButton("undo");
-    private JButton halfUndoButton = new JButton("halfUndo");
-
-    public Main(String title) {
-        super(title);
-
-        canvas.addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                DrawCommand cmd = new DrawCommand(canvas, e.getPoint());
-                history.append(cmd);
-                cmd.execute();
-            }
-        });
-
-        this.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-        clearButton.addActionListener(this);
-        undoButton.addActionListener(this);
-        halfUndoButton.addActionListener(this);
-
-        Box buttonBox = new Box(BoxLayout.X_AXIS);
-        buttonBox.add(clearButton);
-        buttonBox.add(undoButton);
-        buttonBox.add(halfUndoButton);
-        Box mainBox = new Box(BoxLayout.Y_AXIS);
-        mainBox.add(buttonBox);
-        mainBox.add(canvas);
-        getContentPane().add(mainBox);
-
-        pack();
-        setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == clearButton) {
-            history.clear();
-            canvas.repaint();
-        } else if (e.getSource() == undoButton) {
-            history.undo();
-            canvas.repaint();
-        } else if (e.getSource() == halfUndoButton) {
-            history.halfUndo();
-            canvas.repaint();
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
-        new Main("Command Pattern Sample");
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("program.txt"));
+            String text;
+            while ((text = reader.readLine()) != null) {
+                System.out.println("text =  \"" + text + "\"");
+                Node node = new ProgramNode();
+                node.parse(new Context(text));
+                System.out.println("node = " + node);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
 
---
+++ src/interpreter/ParseException.java
@@ -0,0 +1,8 @@
+package interpreter;
+
+public class ParseException extends Exception {
+    public ParseException(String message) {
+        super(message);
+    }
+}
+
